6d752a2bb797a4bf167d7148cb3c3d2d172624a9
Prabhendu Senan <pvsenan@gmail.com>
2020-10-15 14:46:18 +0200

    Initial commit

---
+++ src/main/java/se/pscode/shortenme/ShortenMeApplication.java
@@ -0,0 +1,13 @@
+package se.pscode.shortenme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortenMeApplication.class, args);
+    }
+
+}
